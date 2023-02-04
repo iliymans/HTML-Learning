cb8a4eb1c51feb2e3cc914af0fae88d37b9c1681
Iliya Mansouri <iliyjoon83@gmail.com>
2023-02-04 09:27:56 +0330

    Finish Todo App project

---
+++ ToDo/js/script.js
@@ -0,0 +1,15 @@
+let text = document.querySelector("#input");
+let number = 0;
+function todo() {
+  if (text.value == "") {
+    alert("type something");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = text.value;
+    document.querySelector("ul").appendChild(li);
+    text.value = "";
+    number++;
+    console.log(number);
+    document.querySelector("span").innerHTML = "total :" + number;
+  }
+}
