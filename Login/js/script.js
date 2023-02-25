4c57241c156051f60138713e7a63e4565e06be53
Iliya Mansouri <iliyjoon83@gmail.com>
2023-02-25 17:53:39 +0330

    Finish login project

--- Login/js/script.js
+++ Login/js/script.js
@@ -1,10 +1,16 @@
 let eyeIcon = document.querySelector(".fa-solid");
-let passInput = document.querySelector(".pass");
-if(passInput.value==""){
-  eyeIcon.style.display = "none"
-}else{
-  console.log("object");
-}
+let passInput = document.querySelector("#password");
+let userInput = document.querySelector("#username");
+eyeIcon.style.display = "none";
+
+passInput.addEventListener("keyup", function () {
+  if (passInput.value.length == 0) {
+    eyeIcon.style.display = "none";
+  } else {
+    eyeIcon.style.display = "block";
+  }
+});
+
 eyeIcon.addEventListener("click", function () {
   if (passInput.getAttribute("type") == "password") {
     passInput.setAttribute("type", "text");
@@ -16,3 +22,35 @@ eyeIcon.addEventListener("click", function () {
     eyeIcon.classList.add("fa-eye");
   }
 });
+
+function submitBtn() {
+  if (userInput.value.length == 0) {
+    Swal.fire({
+      icon: "error",
+      text: "Fill out username",
+    });
+  }
+  if (passInput.value.length < 8) {
+    Swal.fire({
+      icon: "error",
+      text: "Password must be minimum 8 characters",
+    });
+  } else if (passInput.value.length > 8 && userInput.value.length > 0) {
+    const Toast = Swal.mixin({
+      toast: true,
+      position: "top-end",
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.addEventListener("mouseenter", Swal.stopTimer);
+        toast.addEventListener("mouseleave", Swal.resumeTimer);
+      },
+    });
+
+    Toast.fire({
+      icon: "success",
+      title: "Signed in successfully",
+    });
+  }
+}
