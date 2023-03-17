e499e7936abcfb95fa30e647ef8b2ab77d36fdb2
Iliya Mansouri <iliyjoon83@gmail.com>
2023-03-17 10:01:15 +0330

    Finish random background generator project

--- random-color-generator/js/script.js
+++ random-color-generator/js/script.js
@@ -3,6 +3,7 @@ let greenValue = document.querySelector("#green");
 let blueValue = document.querySelector("#blue");
 let randomBtn = document.querySelector("#random");
 let body = document.querySelector("body");
+
 redValue.addEventListener("input", function () {
   body.style.backgroundColor = `rgb(${redValue.value} , ${greenValue.value} , ${blueValue.value})`;
 });
@@ -11,4 +12,15 @@ greenValue.addEventListener("input", function () {
 });
 blueValue.addEventListener("input", function () {
   body.style.backgroundColor = `rgb(${redValue.value} , ${greenValue.value} , ${blueValue.value})`;
+});
+
+randomBtn.addEventListener("click", function () {
+  let randomR = Math.floor(Math.random() * 255);
+  let randomG = Math.floor(Math.random() * 255);
+  let randomB = Math.floor(Math.random() * 255);
+  body.style.backgroundColor =
+    "rgb(" + randomR + "," + randomG + "," + randomB + ")";
+  redValue.value = randomR;
+  greenValue.value = randomG;
+  blueValue.value = randomB;
 });
